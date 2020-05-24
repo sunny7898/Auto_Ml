cd8275d39866d737c82ce2de16fdcb7efb6d5322
Sunny Goswami <54610136+sunny7898@users.noreply.github.com>
2020-05-25 02:42:17 +0530

    third

--- ann.py
+++ ann.py
@@ -31,8 +31,6 @@ model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5, batch_si
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy:  %.2f%%" % (scores[1]*100))
-
 model.save("mnist_model.h5")
-
-file1 = open("result.txt","w")
-file1.write(str(scores[1]*100))
+resfile = open("result.txt","w")
+resfile.write(str(scores[1]*100))
